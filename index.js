c2c4a1dcdc79f80ac1788b3a1d139c61eb380de1
Gabriel Bomfim <gabrielbomfimfelipe@gmail.com>
2023-04-18 15:21:30 -0300

    separando em rotas

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const PORT = 3000;
 const express = require("express");
-const bodyParser = require("body-parser");
-const posts = require("./model/posts");
+const apiRoute = require("./routers/api");
 const app = express();
+const path = require("path");
 
-app.get("/all", (req, res) => {
-  res.json(JSON.stringify(posts.getAll()));
-});
 
-app.post("/new", bodyParser.json(), (req, res) => {
-  let title = req.body.title;
-  let description = req.body.description;
-
-  posts.newPost(title, description);
-
-  res.send("Post add");
-});
+app.use("/api", apiRoute);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
