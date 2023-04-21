18e686c1c766a04fb3a28a1607c63476ca436383
Gabriel Bomfim <gabrielbomfimfelipe@gmail.com>
2023-04-21 19:51:39 -0300

    iniciando o script do frontend

---
+++ public/scripts/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  updatePosts();
+});
+function updatePosts() {
+  fetch("http://localhost:5000/api/all")
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      let postElements = "";
+      let posts = JSON.parse(json);
+      posts.forEach((post) => {
+        let postElement = `<div id=${post.id} class="card mb-4">
+                              <div class="card-header">
+                                <h4 class="card-title">${post.title}</h4>
+                              </div>
+                              <div class="card-body">
+                                <div class="card-text">${post.description}</div>
+                              </div>
+                           </div>`;
+        postElements += postElement;
+      });
+      document.getElementById("posts").innerHTML = postElements;
+    });
+}
+
+function newPost() {}
+  
